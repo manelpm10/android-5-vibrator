a43d34a17ab6f0bdaf65f824952b558d950d00fb
Manel <yo@manelperez.com>
2018-06-06 13:28:40 +0200

    Created menu bar with a element to take a picture from cam

--- Readme.md
+++ Readme.md
@@ -1,3 +1,7 @@
+Exercice 1:
 Show a form with a text box that gives a number and a button. If button is clicked the mobile will vibrate the number of times indicated in the text box.
 
 We should use BroadcastReceiver to pass information the number of times to vibrate.
+
+Exercice 2:
+Use mobile camera to take a picture.
--- app/src/main/java/es/pue/android/vibrator/FormActivity.java
+++ app/src/main/java/es/pue/android/vibrator/FormActivity.java
@@ -3,15 +3,23 @@ package es.pue.android.vibrator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class FormActivity extends AppCompatActivity {
+
+    private ImageView ivImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        ivImage = findViewById(R.id.ivImage);
     }
 
     public void letsDance(View view) {
@@ -21,5 +29,21 @@ public class FormActivity extends AppCompatActivity {
         Intent i = new Intent("es.pue.android.vibrator.VIBRATE");
         i.putExtra("numVibrations", numVibrations);
         sendBroadcast(i);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.opCam:
+                Log.i("MENU", "Click!");
+                break;
+        }
+        return true;
     }
 }
