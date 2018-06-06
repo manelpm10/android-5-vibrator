038364cd59fe81c7ee1c538e770624265c19b8ce
Manel <yo@manelperez.com>
2018-06-06 13:16:22 +0200

    Added if for vibration by version

--- app/src/main/java/es/pue/android/vibrator/receiver/VibratorReceiver.java
+++ app/src/main/java/es/pue/android/vibrator/receiver/VibratorReceiver.java
@@ -3,6 +3,8 @@ package es.pue.android.vibrator.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.os.VibrationEffect;
 import android.os.Vibrator;
 
 public class VibratorReceiver extends BroadcastReceiver {
@@ -12,10 +14,15 @@ public class VibratorReceiver extends BroadcastReceiver {
         if (intent.getAction().endsWith("es.pue.android.vibrator.VIBRATE")) {
             int numVibrations = intent.getIntExtra("numVibrations", 0);
 
-            Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-            for (int i = 0; i < numVibrations; i++) {
-                v.vibrate(500);
+                Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+                for (int i = 0; i < numVibrations; i++) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+                    } else {
+                        //deprecated in API 26
+                        v.vibrate(500);
+                    }
+                }
             }
         }
     }
-}
